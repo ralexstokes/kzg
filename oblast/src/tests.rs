726679da91416ac94db18f97264b2c4a21e0d3ee
Alex Stokes <r.alex.stokes@gmail.com>
2021-03-01 16:36:07 -0800

    Refactor oblast to handle ops for `Fr`

--- oblast/src/tests.rs
+++ oblast/src/tests.rs
@@ -19,3 +19,42 @@ fn invalid_pairing() {
         Fr::from_u64(2) * P2::generator(),
     ));
 }
+
+#[test]
+fn can_add() {
+    let x = Fr::from_u64(1001);
+    let y = Fr::from_u64(2002);
+    let sum = x + y;
+    assert_eq!(sum.as_u64(), 3003);
+}
+
+#[test]
+fn can_multiply() {
+    let x = Fr::from_u64(200);
+    let y = Fr::from_u64(3);
+    let product = x * y;
+    assert_eq!(product.as_u64(), 600);
+}
+
+#[test]
+fn can_subtract() {
+    let x = Fr::from_u64(200);
+    let y = Fr::from_u64(10);
+    let result = x - y;
+    assert_eq!(result.as_u64(), 190);
+}
+
+#[test]
+fn can_negate() {
+    let x = Fr::from_u64(200);
+    let sum = x + -x;
+    assert_eq!(sum.as_u64(), 0);
+}
+
+#[test]
+fn can_divide() {
+    let x = Fr::from_u64(200);
+    let y = Fr::from_u64(2);
+    let result = x / y;
+    assert_eq!(result.as_u64(), 100);
+}
